564f1470980f5f1f31c7f8569902e7a98be3255d
soubok <soubok@8cb319d7-811a-0410-9c04-c79b9fbae0fb>
2006-08-27 20:54:19 +0000

    - adaptation for the new non-blocking Socket class (nspr)

--- jsircbot.js
+++ jsircbot.js
@@ -2,309 +2,156 @@
 rfc: rfc2812.txt
 rfc: rfc2813.txt
 */
+
 exec('deflib.js');
-LoadModule('socket')
-//LoadModule('file')
+LoadModule('jsnspr');
 
-//var log = new File('ircbot.log');
+var timeout = new function() {
 
-var NumericCommand = {
-  1:'RPL_WELCOME',
-  2:'RPL_YOURHOST',
-  3:'RPL_CREATED',
-  4:'RPL_MYINFO',
-  5:'RPL_BOUNCE',
-  5:'RPL_MAP',
-  5:'RPL_PROTOCTL',
-  6:'RPL_MAPMORE',
-  7:'RPL_MAPEND',
-  8:'RPL_SNOMASK',
-  9:'RPL_STATMEMTOT',
- 10:'RPL_STATMEM',
- 14:'RPL_YOURCOOKIE',
-200:'RPL_TRACELINK',
-201:'RPL_TRACECONNECTING',
-202:'RPL_TRACEHANDSHAKE',
-203:'RPL_TRACEUNKNOWN',
-204:'RPL_TRACEOPERATOR',
-205:'RPL_TRACEUSER',
-206:'RPL_TRACESERVER',
-207:'RPL_TRACESERVICE',
-208:'RPL_TRACENEWTYPE',
-209:'RPL_TRACECLASS',
-210:'RPL_TRACERECONNECT',
-211:'RPL_STATSLINKINFO',
-212:'RPL_STATSCOMMANDS',
-213:'RPL_STATSCLINE',
-214:'RPL_STATSNLINE',
-215:'RPL_STATSILINE',
-216:'RPL_STATSKLINE',
-217:'RPL_STATSQLINE',
-217:'RPL_STATSPLINE',
-218:'RPL_STATSYLINE',
-219:'RPL_ENDOFSTATS',
-220:'RPL_STATSPLINE',
-221:'RPL_UMODEIS',
-222:'RPL_STATSBLINE',
-223:'RPL_STATSELINE',
-224:'RPL_STATSFLINE',
-225:'RPL_STATSDLINE',
-225:'RPL_STATSZLINE',
-226:'RPL_STATSCOUNT',
-227:'RPL_STATSGLINE',
-231:'RPL_SERVICEINFO',
-232:'RPL_ENDOFSERVICES',
-233:'RPL_SERVICE',
-234:'RPL_SERVLIST',
-235:'RPL_SERVLISTEND',
-239:'RPL_STATSIAUTH',
-240:'RPL_STATSVLINE',
-241:'RPL_STATSLLINE',
-242:'RPL_STATSUPTIME',
-243:'RPL_STATSOLINE',
-244:'RPL_STATSHLINE',
-245:'RPL_STATSSLINE',
-246:'RPL_STATSPING',
-246:'RPL_STATSTLINE',
-246:'RPL_STATSULINE',
-247:'RPL_STATSBLINE',
-247:'RPL_STATSGLINE',
-247:'RPL_STATSXLINE',
-248:'RPL_STATSDEFINE',
-248:'RPL_STATSULINE',
-249:'RPL_STATSDEBUG',
-250:'RPL_STATSDLINE',
-250:'RPL_STATSCONN',
-251:'RPL_LUSERCLIENT',
-252:'RPL_LUSEROP',
-253:'RPL_LUSERUNKNOWN',
-254:'RPL_LUSERCHANNELS',
-255:'RPL_LUSERME',
-256:'RPL_ADMINME',
-257:'RPL_ADMINLOC1',
-258:'RPL_ADMINLOC2',
-259:'RPL_ADMINEMAIL',
-261:'RPL_TRACELOG',
-262:'RPL_TRACEEND',
-262:'RPL_ENDOFTRACE',
-262:'RPL_TRACEPING',
-263:'RPL_TRYAGAIN',
-263:'RPL_LOAD2HI',
-265:'RPL_LOCALUSERS',
-266:'RPL_GLOBALUSERS',
-271:'RPL_SILELIST',
-272:'RPL_ENDOFSILELIST',
-274:'RPL_STATSDELTA',
-275:'RPL_STATSDLINE',
-280:'RPL_GLIST',
-281:'RPL_ENDOFGLIST',
-290:'RPL_HELPHDR',
-291:'RPL_HELPOP',
-292:'RPL_HELPTLR',
-293:'RPL_HELPHLP',
-294:'RPL_HELPFWD',
-295:'RPL_HELPIGN',
-300:'RPL_NONE',
-301:'RPL_AWAY',
-302:'RPL_USERHOST',
-303:'RPL_ISON',
-304:'RPL_TEXT',
-305:'RPL_UNAWAY',
-306:'RPL_NOWAWAY',
-307:'RPL_USERIP',
-307:'RPL_WHOISREGNICK',
-308:'RPL_WHOISADMIN',
-309:'RPL_WHOISSADMIN',
-310:'RPL_WHOISSVCMSG',
-311:'RPL_WHOISUSER',
-312:'RPL_WHOISSERVER',
-313:'RPL_WHOISOPERATOR',
-314:'RPL_WHOWASUSER',
-315:'RPL_ENDOFWHO',
-316:'RPL_WHOISCHANOP',
-317:'RPL_WHOISIDLE',
-318:'RPL_ENDOFWHOIS',
-319:'RPL_WHOISCHANNELS',
-321:'RPL_LISTSTART',
-322:'RPL_LIST',
-323:'RPL_LISTEND',
-324:'RPL_CHANNELMODEIS',
-325:'RPL_UNIQOPIS',
-326:'RPL_NOCHANPASS',
-327:'RPL_CHPASSUNKNOWN',
-329:'RPL_CREATIONTIME',
-331:'RPL_NOTOPIC',
-332:'RPL_TOPIC',
-333:'RPL_TOPICWHOTIME',
-334:'RPL_LISTUSAGE',
-334:'RPL_COMMANDSYNTAX',
-338:'RPL_CHANPASSOK',
-339:'RPL_BADCHANPASS',
-341:'RPL_INVITING',
-342:'RPL_SUMMONING',
-346:'RPL_INVITELIST',
-347:'RPL_ENDOFINVITELIST',
-348:'RPL_EXCEPTLIST',
-349:'RPL_ENDOFEXCEPTLIST',
-351:'RPL_VERSION',
-352:'RPL_WHOREPLY',
-353:'RPL_NAMREPLY',
-354:'RPL_WHOSPCRPL',
-361:'RPL_KILLDONE',
-362:'RPL_CLOSING',
-363:'RPL_CLOSEEND',
-364:'RPL_LINKS',
-365:'RPL_ENDOFLINKS',
-366:'RPL_ENDOFNAMES',
-367:'RPL_BANLIST',
-368:'RPL_ENDOFBANLIST',
-369:'RPL_ENDOFWHOWAS',
-371:'RPL_INFO',
-372:'RPL_MOTD',
-373:'RPL_INFOSTART',
-374:'RPL_ENDOFINFO',
-375:'RPL_MOTDSTART',
-376:'RPL_ENDOFMOTD',
-381:'RPL_YOUREOPER',
-382:'RPL_REHASHING',
-383:'RPL_YOURESERVICE',
-384:'RPL_MYPORTIS',
-385:'RPL_NOTOPERANYMORE',
-391:'RPL_TIME',
-392:'RPL_USERSSTART',
-393:'RPL_USERS',
-394:'RPL_ENDOFUSERS',
-395:'RPL_NOUSERS',
-401:'ERR_NOSUCHNICK',
-402:'ERR_NOSUCHSERVER',
-403:'ERR_NOSUCHCHANNEL',
-404:'ERR_CANNOTSENDTOCHAN',
-405:'ERR_TOOMANYCHANNELS',
-406:'ERR_WASNOSUCHNICK',
-407:'ERR_TOOMANYTARGETS',
-408:'ERR_NOSUCHSERVICE',
-408:'ERR_NOCOLORSONCHAN',
-409:'ERR_NOORIGIN',
-411:'ERR_NORECIPIENT',
-412:'ERR_NOTEXTTOSEND',
-413:'ERR_NOTOPLEVEL',
-414:'ERR_WILDTOPLEVEL',
-415:'ERR_BADMASK',
-416:'ERR_TOOMANYMATCHES',
-416:'ERR_QUERYTOOLONG',
-421:'ERR_UNKNOWNCOMMAND',
-422:'ERR_NOMOTD',
-423:'ERR_NOADMININFO',
-424:'ERR_FILEERROR',
-429:'ERR_TOOMANYAWAY',
-431:'ERR_NONICKNAMEGIVEN',
-432:'ERR_ERRONEUSNICKNAME',
-433:'ERR_NICKNAMEINUSE',
-434:'ERR_SERVICENAMEINUSE',
-435:'ERR_SERVICECONFUSED',
-435:'ERR_BANONCHAN',
-436:'ERR_NICKCOLLISION',
-437:'ERR_UNAVAILRESOURCE',
-437:'ERR_BANNICKCHANGE',
-438:'ERR_DEAD',
-438:'ERR_NICKTOOFAST',
-438:'ERR_NCHANGETOOFAST',
-439:'ERR_TARGETTOOFAST',
-440:'ERR_SERVICESDOWN',
-441:'ERR_USERNOTINCHANNEL',
-442:'ERR_NOTONCHANNEL',
-443:'ERR_USERONCHANNEL',
-444:'ERR_NOLOGIN',
-445:'ERR_SUMMONDISABLED',
-446:'ERR_USERSDISABLED',
-451:'ERR_NOTREGISTERED',
-452:'ERR_IDCOLLISION',
-453:'ERR_NICKLOST',
-455:'ERR_HOSTILENAME',
-461:'ERR_NEEDMOREPARAMS',
-462:'ERR_ALREADYREGISTRED',
-463:'ERR_NOPERMFORHOST',
-464:'ERR_PASSWDMISMATCH',
-465:'ERR_YOUREBANNEDCREEP',
-466:'ERR_YOUWILLBEBANNED',
-467:'ERR_KEYSET',
-468:'ERR_INVALIDUSERNAME',
-468:'ERR_ONLYSERVERSCANCHANGE',
-471:'ERR_CHANNELISFULL',
-472:'ERR_UNKNOWNMODE',
-473:'ERR_INVITEONLYCHAN',
-474:'ERR_BANNEDFROMCHAN',
-475:'ERR_BADCHANNELKEY',
-476:'ERR_BADCHANMASK',
-477:'ERR_MODELESS',
-477:'ERR_NOCHANMODES',
-477:'ERR_NEEDREGGEDNICK',
-478:'ERR_BANLISTFULL',
-479:'ERR_BADCHANNAME',
-481:'ERR_NOPRIVILEGES',
-482:'ERR_CHANOPRIVSNEEDED',
-483:'ERR_CANTKILLSERVER',
-484:'ERR_DESYNC',
-484:'ERR_ISCHANSERVICE',
-485:'ERR_UNIQOPPRIVSNEEDED',
-487:'ERR_CHANTOORECENT',
-488:'ERR_TSLESSCHAN',
-489:'ERR_VOICENEEDED',
-491:'ERR_NOOPERHOST',
-492:'ERR_NOSERVICEHOST',
-501:'ERR_UMODEUNKNOWNFLAG',
-502:'ERR_USERSDONTMATCH',
-503:'ERR_GHOSTEDCLIENT',
-504:'ERR_LAST_ERR_MSG',
-511:'ERR_SILELISTFULL',
-512:'ERR_NOSUCHGLINE',
-512:'ERR_TOOMANYWATCH',
-513:'ERR_BADPING',
-514:'ERR_TOOMANYDCC',
-521:'ERR_LISTSYNTAX',
-522:'ERR_WHOSYNTAX',
-523:'ERR_WHOLIMEXCEED',
-600:'RPL_LOGON',
-601:'RPL_LOGOFF',
-602:'RPL_WATCHOFF',
-603:'RPL_WATCHSTAT',
-604:'RPL_NOWON',
-605:'RPL_NOWOFF',
-606:'RPL_WATCHLIST',
-607:'RPL_ENDOFWATCHLIST',
-617:'RPL_DCCSTATUS',
-618:'RPL_DCCLIST',
-619:'RPL_ENDOFDCCLIST',
-620:'RPL_DCCINFO',
-999:'ERR_NUMERIC_ERR'
+	var _min;
+	var _tlist = {};
+	this.Add = function( time, func ) {
+	
+		var when = IntervalNow() + time;
+		while( _tlist[when] ) when++; // avoid same time
+		_tlist[when] = func;
+		if ( when < _min )
+			_min = when;
+		return when;
+	}
+	
+	this.Remove = function(when) {
+		
+		if ( when == _min )
+			_min = Number.POSITIVE_INFINITY;
+		delete _tlist[when];
+	}
+	
+	this.Next = function() {
+		
+		_min = Number.POSITIVE_INFINITY;
+		for ( var w in _tlist )
+			if ( w < _min )
+				_min = w;
+		return _min == Number.POSITIVE_INFINITY ? undefined : _min - IntervalNow();
+	}
+
+	this.Process = function() {
+		
+		var now = IntervalNow();
+		if ( _min > now )
+			return;
+		for ( var [w,f] in _tlist )
+			if ( w <= now ) {
+				f();
+				delete _tlist[w];
+			}
+	}
 }
+
+
+var io = new function() {
+	
+	var _descriptorList = [];
+	
+	this.AddTimeout = function( time, fct ) {
+	
+		timeout.Add( time, fct );
+	}
+	
+	this.AddDescriptor = function( d ) {
+	
+		_descriptorList.push(d);
+	}
+
+	this.RemoveDescriptor = function(d) {
+		
+		_descriptorList.splice( _descriptorList.indexOf(d), 1 );
+	}
+
+	this.Process = function( endPredicate ) {
+	
+		for ( ; !endPredicate() ; ) {
+
+			Poll(_descriptorList, timeout.Next() || 1000);
+			timeout.Process();
+		}
+
+	}
+	
+	this.Close = function() {
+		
+		for ( var [i,d] in _descriptorList )
+			d.Close();		
+	
+	}
+
+}
+
+
+var log = new function() {
+
+	var _file = new File('ircbot.log');
+	_file.Open( File.CREATE_FILE + File.WRONLY + File.APPEND );
+	
+	this.Write = function( data ) {
+		
+		_file.Write( data );
+	}
+	
+	this.WriteLn = function( data ) {
+		
+		this.Write( data + '\n' );
+	}
+
+	this.Close = function() {
+		
+		_file.Close();
+	}
+}
+
+
+///////////////////////////////////////////////////////
+
+
+var NumericCommand = exec('NumericCommand.js');
 
 function Time() {
 
-	return (new Date()).getTime(); // in ms
+	//return (new Date()).getTime(); // in ms
+	return IntervalNow();
 }
 
 function FloodControlSender( max, time, rawSender ) {
-	
-	var _count = 0;
+
+	var _count = max;
 	var _time = Time();
-	var _lines = [];
+	var _queue = [];
+	
+	function Process() {
+	
+		print('.');
+	
+		var buffer = '';
+		for ( ; _count > 0 && _queue.length > 0 ; _count--) {
+			buffer += _queue.shift();
+		}
+		buffer != '' && rawSender(buffer);
+	}
+	
+	function timeout() {
+
+		_count = max;
+		Process();
+		io.AddTimeout( time, timeout );
+	}
+	
+	io.AddTimeout( time, timeout );
 	
 	this.Send = function( line ) {
 		
-		if ( line != undefined )
-			_lines.push(line);
-		
-		if ( Time() - _time >= time ) {
-			
-			_time = Time();
-			_count = 0;
-		}
-
-		for ( var buffer = '';  _count < max && _lines.length > 0; _count++ )
-			buffer += _lines.shift();
-
-		buffer != '' && rawSender(buffer);
+		line != undefined && _queue.push(line);
+		Process();
 	}
 }
 // ( 15/16 bytes messages )
@@ -322,8 +169,11 @@ function FloodControlSender( max, time, rawSender ) {
 
 
 function IRCClient( server, port ) {
-	
+
+	var _this = this;
+	var _startTime = Time();
 	var _socket;
+	var _receiveBuffer = '';	
 	var _sender = new FloodControlSender( 1, 1000, function(buffer) { _socket.Send( buffer ) } );
 	var _startTime;
 	var _modules = [];
@@ -332,7 +182,7 @@ function IRCClient( server, port ) {
 
 	this.Send = function( message ) {
 
-		print( '> '+message );
+		log.WriteLn( '> '+message );
 
 		//_socket.Send( message+'\r\n' );
 		_sender.Send( message+'\r\n' );
@@ -356,11 +206,11 @@ function IRCClient( server, port ) {
 
 	this.OnMessage = function( message ) {
 		
-		print( 'raw message: '+message+'\r\n');
+		log.WriteLn( 'raw message: ' + message );
 
 		var prefix = message.indexOf( ':' );
 		var trailing = message.indexOf( ':', 1 );
-		args = message.substring( prefix?0:1, trailing>0?trailing-1:message.length ).split(' ');
+		var args = message.substring( prefix?0:1, trailing>0?trailing-1:message.length ).split(' ');
 		if (prefix)
 			args.unshift( undefined );
 		if ( trailing>0 )
@@ -368,48 +218,59 @@ function IRCClient( server, port ) {
 		if ( !isNaN( args[1] ) )
 			args[1] = NumericCommand[Number(args[1])];
 
-		print( '< '+args );
+		log.WriteLn( '< '+args );
 
 		this.DispatchCommand.apply( this, args );
 	}
 
-	this.Create = function() {
 
-		var _receiveBuffer = '';
+	this.onSocketReadable = function(s) {
+
+		var buf = _socket.Recv();
+		if ( buf.length == 0 ) {
+	
+			_this.DispatchEvent( 'OnDisconnect' );
+			delete s.readable;
+			s.Close();
+			io.RemoveDescriptor(s);
+			return;
+		}
+
+		_receiveBuffer += buf;
+
+		var eol;
+		while ( (eol = _receiveBuffer.indexOf('\r\n')) != -1 ) {
+
+			_this.OnMessage( _receiveBuffer.substring( 0, eol ) )
+			_receiveBuffer = _receiveBuffer.substring( eol+2 ); // +2 for '\r\n';
+		}
+	}
+
+	this.Create = function() {
 
 		_socket = new Socket();
 		_socket.Connect( server, port );
-
-		var _startTime = Time();
-
-		this.DispatchEvent( 'OnConnect' );
-
-		for ( var l = 0; _socket.isConnected ;++l ) {
-
-			_sender.Send();
-			if ( Socket.WaitForReceive( 100, _socket ) ) {
-				
-				_receiveBuffer += _socket.Receive();
-				
-				var eol;
-				while ( (eol = _receiveBuffer.indexOf('\r\n')) != -1 ) {
-
-					this.OnMessage( _receiveBuffer.substring( 0, eol ) )
-					_receiveBuffer = _receiveBuffer.substring( eol+2 ); // +2 for '\r\n';
-				}
-			}
+		_socket.writable = function() {
+		
+			delete _socket.writable;
+	
+			_socket.readable = _this.onSocketReadable;
+			_this.DispatchEvent( 'OnConnect' );
 		}
-		this.DispatchEvent( 'OnDisconnect' );
+		io.AddDescriptor(_socket);
 	}
-	
-	
-	
+
 
 	this.AddModule = function( mod ) {
 		
 		mod.Send = this.Send;
 		mod.data = _data;
 		_modules.push( mod );
+	}
+	
+	this.Quit = function() {
+	
+		this.Send( 'QUIT :');
 	}
 }
 
@@ -672,7 +533,18 @@ var client = new IRCClient( 'euroserv.fr.quakenet.org', 6667 );
 client.AddModule( new DefaultModule( 'cdd_etf_bot' ) ); 
 client.AddModule( new ChanModule( '#soubok' ) );
 
+
 client.Create();
+
+io.Process( function() { return endSignal } );
+print('\nbreak!\n');
+
+client.Quit();
+
+//io.Close();
+
+log.WriteLn(' ========================================================= END ========================================================= ');
+log.Close();
 
 
 // My notes
